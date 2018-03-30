18095f40fdbd28fa5c956be18e49101e472476e2
Prodev2017 <chris.bergman320@gmail.com>
2018-03-30 03:56:08 -0400

    add getting paid

---
+++ application/views/getting_paid/index.php
@@ -0,0 +1,36 @@
+<?php
+	$this->load->view('layout/header');
+?>
+		
+		<div class="main-section">
+
+			<section class="tab-current">
+
+				<h2>GETTING PAID</h2>
+
+				[getting paid content here]
+
+				<div class="clearfix"></div>
+
+			</section><!-- end of tab current -->
+			
+			<div class="planned-box">
+			
+				<section class="tab-planned">
+					<span>Planned</span>
+					<span class="oval"></span>
+				</section><!-- end of tab planned -->
+				
+			</div><!-- end of planned box -->
+			
+			<section class="tab-past">
+				<span>Past</span>
+				<span class="oval"></span>
+			</section><!-- end of tab past -->
+
+		</div><!-- end of form block -->
+									
+<?php
+	$this->load->view('layout/sidebar');
+	$this->load->view('layout/footer');
+?>
