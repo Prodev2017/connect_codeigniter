f7abb55f61e6385635d66623400d15d7f2ccbd00
Prodev2017 <chris.bergman320@gmail.com>
2018-03-31 03:12:53 -0400

    add search

---
+++ application/views/search/index.php
@@ -0,0 +1,19 @@
+<?php
+	$this->load->view('layout/header');
+?>
+		
+		<div class="main-section-full">
+
+			<section class="tab-current">
+
+				<h2>Search</h2>
+
+				[Search content here]
+
+				<div class="clearfix"></div>
+
+			</section><!-- end of tab current -->
+									
+<?php
+	$this->load->view('layout/footer');
+?>
